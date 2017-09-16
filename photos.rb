4d4b23fd3d536bc8704b56d622dc7187a10c3b66
Tom <trmoir9@gmail.com>
2017-09-16 15:44:56 +0100

    Uploaded improved version
    Created the program as a class, removed needless loops, and refactored.

--- photos.rb
+++ photos.rb
@@ -1,121 +1,108 @@
-home = 'C:/Users/Tom/Pictures'
-iphone = 'C:/Users/Tom/Pictures/iPhone Pics'
-
-def get_img_num string_n
-	n = string_n.to_i
-	if n < 10
-		return IMG_"000#{n}.JPG"
-	elsif n < 100
-		return "IMG_00#{n}.JPG"
-	elsif n < 1000
-		return "IMG_0#{n}.JPG"
-	else
-		return "IMG_#{n}.JPG"
+class RenamePics
+	def initialize
+		@root = 'C:/Users/Tom/Pictures'
+		@iphone = 'C:/Users/Tom/Pictures/iPhone Pics'
+		puts "We'll start by selecting the range of pictures you wish to move and rename."
+		get_range
 	end
-end
 
-puts "We'll start by selecting the range of pictures you wish to move and rename."
-
-while true
-	puts "Please enter the IMG number of the first picture."
-	first_pic_num = gets.chomp
-	puts
-	int_check = (first_pic_num.to_i.to_s == first_pic_num)
-	if int_check != true
-		puts "Not a valid IMG number"
-	else
-		first_pic = get_img_num first_pic_num
-		Dir.chdir iphone
-		if File.exists?(first_pic) == false
-			puts "Image number #{first_pic} not found."
-			puts
-		else
-			break
+	def get_img_num(string_n)
+		n = string_n.to_i
+		case
+			when n < 10 then "IMG_000#{n}.JPG"
+			when n < 100 then "IMG_00#{n}.JPG"
+			when n < 1000 then "IMG_0#{n}.JPG"
+			else "IMG_#{n}.JPG"
 		end
 	end
-end
 
-puts
-puts "Great! Next step:"
-
-while true
-	puts "Please enter the IMG number of the final picture."
-	last_pic_num = gets.chomp
-	puts
-	
-	if last_pic_num.to_i < first_pic_num.to_i
-		abort("ERROR: Last IMG number must be less than the first IMG number.")
-	end
-	
-	int_check = (last_pic_num.to_i.to_s == last_pic_num)
-	if int_check != true
-		puts "Not a valid IMG number"
-	else
-		last_pic = get_img_num last_pic_num
-		Dir.chdir iphone
-		if File.exists?(last_pic) == false
-			puts "Image number #{last_pic} not found."
-		else
-			break
-		end
-	end
-end
-
-puts "Excellent!  We have selected #{first_pic} to #{last_pic} as our batch of pictures."
-batch = ''
-Dir.chdir home
-
-while Dir.exists?(batch) != true
-	puts "What shall we call this batch?"
-	batch = gets.chomp
-	puts
-	if Dir.exists?(batch) != true
-		puts "No folder called #{batch} was found."
-		puts "Would you like to create one now? (Y/N)"
-		while true
-			confirm = gets.chomp
-			puts
-			if (confirm.upcase == 'Y') || (confirm.upcase == 'YES')
-				Dir.mkdir(batch)
-				count = 0
-				break
-			elsif (confirm.upcase == 'N') || (confirm.upcase == 'NO')
-				puts "Folder wasn't created"
-				break
+	def get_num
+		num = gets.chomp
+		puts
+		if num == num.to_i.to_s
+			Dir.chdir(@iphone)
+			if File.exists?(get_img_num(num))
+				num.to_i
 			else
-				puts "Please answer Y/N"
+				puts "File #{get_img_num(num)} not found. Please try again."
+				get_num
+			end
+		else
+			puts "Please enter a valid number"
+			get_num
+		end
+	end
+
+	def confirm
+		answer = gets.chomp
+		case answer.downcase
+		when 'y' || 'yes' then true
+		when 'n' || 'no' then false
+		else
+			puts "Please answer y / n"
+			confirm
+		end
+	end
+
+	def get_range
+		puts "Please enter the IMG number of the first picture."
+		@first_pic_num = get_num
+		puts "Great! Now please enter the IMG number of the final picture."
+		@last_pic_num = get_num
+		if @first_pic_num > @last_pic_num
+			temp = @last_pic_num
+			@last_pic_num = @first_pic_num
+			@first_pic_num = temp
+		end
+		puts "Excellent!  We have selected #{get_img_num(@first_pic_num)} to #{get_img_num(@last_pic_num)} as our batch of pictures."
+		puts "Is this correct? [y / n]"
+		confirm ? name_batch : get_range
+	end
+
+	def name_batch
+		Dir.chdir(@root)
+		puts "Which folder would you like to save this batch to?"
+		batch = gets.chomp.strip
+		if batch == ''
+			puts "Please enter a valid folder name"
+			name_batch
+		elsif File.exists?(batch)
+			puts "A folder called #{batch} already exists. Would you like to save here? [y / n]"
+			confirm ? save(batch) : name_batch
+		else
+			puts "No folder called #{batch} was found. Would you like to create it now? [y / n]"
+			if confirm
+				Dir.mkdir(batch)
+				save(batch)
+			else
+				name_batch
 			end
 		end
-	else
-		puts "Folder called #{batch} already exists."
+	end
+
+	def save(batch_name)
+		folder = "#{@root}/#{batch_name}"
+		current_pics = Dir["#{folder}/*.JPG"]
+		new_num = current_pics.empty? ? 1 : current_pics.size + 1
+		puts "Saving pictures to #{folder}..."
+		iter = @first_pic_num
+		count = 0
+		while iter <= @last_pic_num
+			picture = get_img_num(iter)
+			Dir.chdir(@iphone)
+			if File.exists?(picture)
+				Dir.chdir(folder)
+				File.rename("#{@iphone}/#{picture}", "#{batch_name} #{new_num}.JPG")
+				new_num += 1
+				iter += 1
+				count += 1
+			else
+				iter += 1
+			end
+		end
+		puts
+		puts "Complete! #{count} pictures were moved to #{folder}"
 	end
 end
 
-new_folder = "#{home}/#{batch}"
-puts "Using destination:  #{new_folder}"
-
-existing_pics = Dir["#{new_folder}/*.JPG"]
-if existing_pics.length > 0
-	count = existing_pics.length
-end
-
-progress = first_pic_num.to_i
-
-while progress <= last_pic_num.to_i
-	picture = get_img_num progress
-	Dir.chdir iphone
-	if File.exists?(picture) == true
-		count = count + 1
-		locate = "#{iphone}/#{picture}"
-		new_name = "#{batch} #{count}.JPG"
-		Dir.chdir new_folder
-		File.rename locate, new_name
-		progress = progress + 1
-	else
-		progress = progress + 1
-	end
-end
-
-puts
-puts 'Complete!'
-puts "#{count} file(s) were moved to #{new_folder}"
+RenamePics.new
